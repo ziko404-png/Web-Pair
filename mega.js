0e2cf828e114c12f070b09cb520e6375837c4dcd
ziko404-png <ziko404error@gmail.com>
2026-06-02 13:21:09 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-  email: "Enter your mega email",
-  password: "Enter your mega password",
+  email: "ziko404error@gmail.com",
+  password: "Enter Your Mega Password",
   userAgent:
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246",
 };
